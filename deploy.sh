dabd66040aa22cbae1c07f60e41b994efdb0fb07
andypaxo <andy@andypaxo.net>
2013-03-27 18:11:20 -0600

    Script to deploy to github

---
+++ deploy.sh
@@ -0,0 +1,19 @@
+# Compile
+mkdir -p bin
+~/Development/air3-6/bin/mxmlc -output bin/cave.swf -source-path flixel -- src/Cave.as
+
+if [ $? -eq 0 ]
+	then 
+
+	# Copy to deploy dir
+	cp -f bin/cave.swf ../cave-doc/bin/cave.swf
+
+	pushd ../cave-doc
+
+	# Cachebuster
+	sed s/{cb}/`date +%s`/ gameframe-tmpl.html > gameframe.html
+
+	# Publish
+	git commit -am "New version"
+	git push
+fi
